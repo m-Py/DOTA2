e4283be96ef0a610ffbb53d962a6a94b15f86615
Martin Papenberg <martin.papenberg@hhu.de>
2018-03-13 11:44:42 +0100

    added default color to error bars

--- R/plot_criterion.R
+++ R/plot_criterion.R
@@ -155,7 +155,7 @@ ci95 <- function(x, na.rm = FALSE) {
 #' @export
 #' 
 error_bar <- function(x, y, upper, lower=upper, length=0.1, width = 1.5,
-                      col) {
+                      col = "black") {
     if(length(x) != length(y) | length(y) !=length(lower) | length(lower) != length(upper))
         stop("vectors must be same length")
     arrows(x,y+upper, x, y-lower, angle=90, code=3, length=length, lwd = width, col=col)
