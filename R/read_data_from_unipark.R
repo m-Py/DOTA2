91a12529623404411f22d4141d9eb6fc0a5fb5c9
Martin Papenberg <martin.papenberg@hhu.de>
2018-10-30 13:03:59 +0100

    removed two unnecessary arguments from function

--- R/read_data_from_unipark.R
+++ R/read_data_from_unipark.R
@@ -6,13 +6,6 @@
 #
 # @param JsonString The JSON string, usually stored in the column of an
 #     Unipark-exported csv
-# @param itemIDs A numeric vector corresponding to the IDs of all items
-#     to be exported. Change only if only a subset of items is to be
-#     returned; otherwise do not change the default value `NULL`.
-# @param itemPrefix By default the function assumes that each item is
-#     represented in the JSON string by the name "itemx" where x is the
-#     ID of the item. If the prefix is anything other than "item", this
-#     argument can be used to tell the function this prefix.
 #
 # @return A \code{list} containing the test data for one test taker
 #   \item{item1 ... itemx}{Item data for the x test items}
@@ -23,13 +16,9 @@
 # @author Martin Papenberg \email{martin.papenberg@@hhu.de}
 #
 
-get_response_data_person <- function(JsonString, itemIDs = NULL,
-                                     itemPrefix = "item") {
+get_response_data_person <- function(JsonString) {
   jsonList       <- convertJsonToList(JsonString)
   domcItemData   <- getDomcItemData(jsonList)
-  #sortedIDs      <- getItemsIDsSorted(domcItemData, itemPrefix)
-  #selectionIDs   <- selectIDs(sortedIDs, itemIDs)
-  #sortedItemData <- sortItemData(domcItemData, selectionIDs, itemPrefix)
   return(domcItemData)
 }
 
@@ -37,7 +26,7 @@ get_response_data_person <- function(JsonString, itemIDs = NULL,
 convertJsonToList <- function(JsonString) {
   # had to convert "" to '' in Unipark, this conversion needs to be reverted:
   JsonString <- gsub("'", "\"", JsonString)
-  jsonList <- jsonlite::fromJSON(JsonString, simplifyDataFrame=FALSE)
+  jsonList <- jsonlite::fromJSON(JsonString, simplifyDataFrame = FALSE)
   return(jsonList)
 }
 
